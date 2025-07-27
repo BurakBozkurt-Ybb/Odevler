9be135070e6e85c85834c161c51e876b9c76fc52
monolesh <burakbozkurtybb@gmail.com>
2025-07-27 10:55:01 +0300

    java-lesson

---
+++ src/main/java/org/example/IslemSirasiHesaplama.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class IslemSirasiHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // a, b, c değerlerini alındığı kısım
+        System.out.print("Birinci sayıyı (a) giriniz: ");
+        int a = input.nextInt();
+
+        System.out.print("İkinci sayıyı (b) giriniz: ");
+        int b = input.nextInt();
+
+        System.out.print("Üçüncü sayıyı (c) giriniz: ");
+        int c = input.nextInt();
+
+        // İşlem: a + b * c - b
+        int sonuc = a + b * c - b;
+
+        // Sonucu ekrana yazdır
+        System.out.println("İşlem sonucu: " + sonuc);
+
+        input.close();
+    }
+}
